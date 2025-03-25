0e0af0032cb695c2fdbfbc682f171a21aeb5800a
Scottyboa <scott_no35@hotmail.com>
2025-03-25 01:34:29 +0100

    Update main-index.js

--- js/main-index.js
+++ js/main-index.js
@@ -2,7 +2,7 @@ import { initIndexLanguage } from './languageLoaderUsage.js';
 import { initConsentBanner, initInfoModals } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  // Initialize language support for the index page.
+  // Initialize language support for the index page (this now includes the new "offerText" update)
   await initIndexLanguage();
 
   // Initialize the consent banner.
