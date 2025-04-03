6e9ab765bbb17797e453ac7b41cebcc0bf0aae6a
Scottyboa <scott_no35@hotmail.com>
2025-04-03 12:58:29 +0200

    Update language-it.js

--- js/language/language-it.js
+++ js/language/language-it.js
@@ -105,8 +105,6 @@ Per far funzionare la webapp, il tuo portafoglio OpenAI deve avere fondi suffici
     </ul>
   </li>
 </ol>
-<h2>Opzione Alternativa per la Generazione delle Note</h2>
-<p>Se possiedi già un account OpenAI, puoi utilizzare la generazione delle note tramite ChatGPT sul tuo profilo — che è sostanzialmente gratuita. In tal caso, sostieni solo il costo della trascrizione da voce a Testo quando utilizzi questa webapp.</p>
 <h2>Flessibilità d'Uso</h2>
 <p>A differenza dei fornitori che richiedono un abbonamento mensile, paghi solo per l'utilizzo. Se prendi una giornata libera, vai in vacanza o se non utilizzi il servizio, il costo è zero. Anche se usi il servizio ogni giorno per tutte le consultazioni con i pazienti, il costo per ogni consultazione rimane notevolmente inferiore rispetto ad altri fornitori.</p>
 <hr>
